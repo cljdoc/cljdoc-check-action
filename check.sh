7f8c7794e3dc41ddacd56aaea7cdd4f58a425a6c
Jakub Holy <jakubholy@jakubholy.net>
2022-03-07 22:23:00 +0100

    report clj errors to stderr, not file

--- check.sh
+++ check.sh
@@ -9,8 +9,8 @@ if [ "$action" = "analyze" ]; then
     echo "Running 'analyze :project $mvn_project :version $mvn_version' to check a libary installed into a Maven repo"
     [ -n "$mvn_project" ] || die 'Missing argument mvn_project that is required for action=analyze'
     [ -n "$mvn_version" ] || die 'Missing argument mvn_version that is required for action=analyze'
-    clojure -Tcljdoc-analyzer analyze :download true :project \""$mvn_project"\" :version \""$mvn_version"\"
+    clojure --report stderr -Tcljdoc-analyzer analyze :download true :project \""$mvn_project"\" :version \""$mvn_version"\"
 else
-  echo "Running 'analyze-local' to check the deps.edn based project in the current directory..."
-  clojure -Tcljdoc-analyzer analyze-local
+  echo "Running 'analyze-local' to check the deps.edn based project in the current directory $(pwd)..."
+  clojure --report stderr -Tcljdoc-analyzer analyze-local
 fi
