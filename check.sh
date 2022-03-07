22d398f85470197744fb07a9d0fb3a0c04b1e072
Jakub Holy <jakubholy@jakubholy.net>
2022-03-07 18:07:54 +0100

    cljdoc -> cljdoc-analyzer

--- check.sh
+++ check.sh
@@ -9,8 +9,8 @@ if [ "$action" -eq "analyze" ]; then
     echo "Running 'analyze :project $mvn_project :version $mvn_version' to check a libary installed into a Maven repo"
     [ -n "$mvn_project" ] || die 'Missing argument mvn_project that is required for action=analyze'
     [ -n "$mvn_version" ] || die 'Missing argument mvn_version that is required for action=analyze'
-    clojure -Tcljdoc :download true analyze :project \""$mvn_project"\" :version \""$mvn_version"\"
+    clojure -Tcljdoc-analyzer :download true analyze :project \""$mvn_project"\" :version \""$mvn_version"\"
 else
   echo "Running 'analyze-local' to check the deps.edn based project in the current directory..."
-  clojure -Tcljdoc analyze-local
+  clojure -Tcljdoc-analyzer analyze-local
 fi
